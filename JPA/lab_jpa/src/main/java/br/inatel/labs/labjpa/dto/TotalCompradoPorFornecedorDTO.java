08794305a53dc88f6fb68858d60819a502295dc2
gustavoaesteves <gustavoalves19992@gmail.com>
2022-11-05 10:32:25 -0300

    Projeto JPA, está na sua propria pasta, tive problemas com o Eclipse em fazer no mesmo workspace

---
+++ JPA/lab_jpa/src/main/java/br/inatel/labs/labjpa/dto/TotalCompradoPorFornecedorDTO.java
@@ -0,0 +1,28 @@
+package br.inatel.labs.labjpa.dto;
+
+import java.math.BigDecimal;
+
+public class TotalCompradoPorFornecedorDTO {
+
+    private String fornecedorRazaoSocial;
+
+    private BigDecimal totalComprado;
+
+    public TotalCompradoPorFornecedorDTO(String fornecedorRazaoSocial, BigDecimal totalComprado) {
+        this.fornecedorRazaoSocial = fornecedorRazaoSocial;
+        this.totalComprado = totalComprado;
+    }
+
+    public String getFornecedorRazaoSocial() {
+        return fornecedorRazaoSocial;
+    }
+
+    public BigDecimal getTotalComprado() {
+        return totalComprado;
+    }
+
+    @Override
+    public String toString() {
+        return "[fornecedorRazaoSocial=" + fornecedorRazaoSocial + ", totalComprado="+ totalComprado + "]";
+    }
+}
